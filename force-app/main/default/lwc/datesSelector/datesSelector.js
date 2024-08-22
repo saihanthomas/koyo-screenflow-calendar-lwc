fe0912a02ca77c76ff32bfab5095555d906218d0
saihanthomas <sai@thomas-gr.com>
2024-08-22 16:01:24 +0900

    japanese localization

--- force-app/main/default/lwc/datesSelector/datesSelector.js
+++ force-app/main/default/lwc/datesSelector/datesSelector.js
@@ -4,6 +4,33 @@ import flatpickrjs from '@salesforce/resourceUrl/flatpickrjs';
 import flatpickrcss from '@salesforce/resourceUrl/flatpickrcss';
 
 export default class DatesSelector extends LightningElement {
+    japaneseLocale = {
+        weekdays: {
+            shorthand: ["日", "月", "火", "水", "木", "金", "土"],
+            longhand: [
+                "日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"
+            ]
+        },
+        months: {
+            shorthand: [
+                "1月", "2月", "3月", "4月", "5月", "6月",
+                "7月", "8月", "9月", "10月", "11月", "12月"
+            ],
+            longhand: [
+                "1月", "2月", "3月", "4月", "5月", "6月",
+                "7月", "8月", "9月", "10月", "11月", "12月"
+            ]
+        },
+        firstDayOfWeek: 0, // Start the week on Sunday
+        time_24hr: true,
+        rangeSeparator: ' から ',
+        monthAriaLabel: '月',
+        amPM: ['午前', '午後'],
+        yearAriaLabel: '年',
+        hourAriaLabel: '時間',
+        minuteAriaLabel: '分'
+    };
+
     renderedCallback() {
         Promise.all([
             loadScript(this, flatpickrjs),
@@ -21,6 +48,7 @@ export default class DatesSelector extends LightningElement {
         const element = this.template.querySelector('.flatpickr');
         flatpickr(element, {
             inline: true, // Set the calendar to be always visible
+            locale: this.japaneseLocale,
             dateFormat: "Ymd",
             mode: "multiple",
             onChange: this.handleDateChange.bind(this)
